3fe358baeee21a17b40353fcd9ed87bed3ff7c46
josspeve <josspeve@gmail.com>
2025-08-31 15:38:02 -0600

    agregar archivos tarea 2 y def saludar

---
+++ def saludar().py
@@ -0,0 +1,15 @@
+from unittest import result
+
+
+def greetings():
+     print("¡Hola,mundo!")
+     
+     
+greetings()
+
+def sum(a, b):
+    c = 3
+    return a + b + c
+
+result = sum(3, 5)
+print(result)
